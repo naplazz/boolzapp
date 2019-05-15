3f62f95d82efd1f7187a98f6774aef6f3bcd53ac
naplazz <naplazz@gmail.com>
2019-05-15 15:09:46 +0200

    fine

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
+$('.inputcont .fa-search').click(function() {
+  var srcText = $("#searchText").val();
+
+  $(".chat-history-element").hide()
+
+  $(".chat-history-element").each(function() {
+    if ($(this).find('.nome').text() == srcText) {
+      $(this).show();
+
+    } else {
+      $(this).hide();
+    }
+
+  });
+
+});
+
 $('#sendBtn').click(function() {
   var userMsg = $('#inputMsg').val();
   var optionVal = $(".selectSender option:selected").val();
 
-  if (optionVal === "user") {
-    $('.column-content-right').append('<div class="msgUtente msg">' +'<span>' + userMsg + '</span>' +'</div>');
+  if (userMsg) {
+    if (optionVal === "user") {
+      $('.column-content-right').append('<div class="msgUtente msg">' + '<span>' + userMsg + '</span>' + '</div>');
+      setTimeout(function() {
+        $('.column-content-right').append('<div class="msgInterlocutore msg">' + '<span>' + userMsg + '<span>' + '</div>');
+      }, 1000);
+    } else if (optionVal === "interlocutore") {
+      $('.column-content-right').append('<div class="msgInterlocutore msg">' + '<span>' + userMsg + '<span>' + '</div>');
+      $('.column-content-right').append('<div class="msgUtente msg">' + '<span>' + userMsg + '</span>' + '</div>');
+    } else {
+      alert('Non hai selezionato chi invia il messaggio');
+    };
+    $('#inputMsg').val('');
 
-  } else if (optionVal === "interlocutore"){
-    $('.column-content-right').append('<div class="msgInterlocutore msg">' +'<span>' + userMsg+ '<span>' + '</div>');
   } else {
-    alert('Non hai selezionato chi invia il messaggio');
-  };
-  $('#inputMsg').val('');
+    alert('messaggio vuoto')
+  }
+
 });
