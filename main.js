1864cc6f4241ae901a7fcea0e1d922144be86f09
naplazz <naplazz@gmail.com>
2019-05-16 20:11:02 +0200

    modifica ricerca

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-$('.inputcont .fa-search').click(function() {
-  var srcText = $("#searchText").val();
+
+
+
+$(".chat-history-element").click(function() {
+  $(".chat-history-element").removeClass('selected');
+  $(this).addClass('selected');
+
+});
+
+$('#searchText').keyup(function() {
+  var srcText = $("#searchText").val().toLowerCase();
 
   $(".chat-history-element").hide()
 
   $(".chat-history-element").each(function() {
-    if ($(this).find('.nome').text() == srcText) {
+    if ($(this).find('.nome').text().toLowerCase().includes(srcText)) {
       $(this).show();
 
     } else {
@@ -12,23 +21,24 @@ $('.inputcont .fa-search').click(function() {
     }
 
   });
-
 });
 
 $('#sendBtn').click(function() {
   var userMsg = $('#inputMsg').val();
   var optionVal = $(".selectSender option:selected").val();
+  var userMessage =  '<div class="msgUtente msg">' + '<span>' + userMsg + '</span>' + '</div>';
+  var interlocutoreMessage = '<div class="msgInterlocutore msg">' + '<span>' + 'Ciao, messaggio ricevuto!' + '<span>' + '</div>';
 
   if (userMsg) {
     if (optionVal === "user") {
-      $('.column-content-right').append('<div class="msgUtente msg">' + '<span>' + userMsg + '</span>' + '</div>');
+      $('.column-content-right').append(userMessage);
       setTimeout(function() {
-        $('.column-content-right').append('<div class="msgInterlocutore msg">' + '<span>' + 'Ciao, messaggio ricevuto!' + '<span>' + '</div>');
+        $('.column-content-right').append(interlocutoreMessage);
       }, 1000);
     } else if (optionVal === "interlocutore") {
-      $('.column-content-right').append('<div class="msgInterlocutore msg">' + '<span>' + userMsg + '<span>' + '</div>');
+      $('.column-content-right').append(userMessage);
       setTimeout(function() {
-        $('.column-content-right').append('<div class="msgUtente msg">' + '<span>' + 'Ciao, messaggio ricevuto!' + '</span>' + '</div>');
+        $('.column-content-right').append(interlocutoreMessage);
       }, 1000);
     } else {
       alert('Non hai selezionato chi invia il messaggio');
